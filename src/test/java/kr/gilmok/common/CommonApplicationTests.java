b8eb817e3ac3865b6b1e6a3c4748fe0376aef157
gunheecha <gunhee2326@naver.com>
2026-02-05 14:51:41 +0900

    SCRUM-1: ì´chore : 1-add-template

---
+++ src/test/java/kr/gilmok/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gilmok.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
