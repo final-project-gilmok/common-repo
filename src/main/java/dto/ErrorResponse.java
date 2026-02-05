37b8f714717c5f1560f446fe1905ae239fd94873
gunheecha <gunhee2326@naver.com>
2026-02-05 15:26:10 +0900

    feat : 공통 속성 추가
    API 공통 응답 코드 추가
    Auth 유저 정보 dto 추가
    종속성 라이브러리로 변경
    커스텀 공통 예외 코드 추가
    에러 공통 코드 추가
    에러 공통 응답 코드 추가
    JWT 유틸 파일 추가
    
    Closes : #1

---
+++ src/main/java/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String status;   // "error" 고정
+    private String code;     // "U001", "S001" 등 에러 코드
+    private String message;  // "잘못된 입력입니다" 등 메시지
+
+    public static ErrorResponse of(String code, String message) {
+        return new ErrorResponse("error", code, message);
+    }
+}
