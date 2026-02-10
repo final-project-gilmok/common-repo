195c73457c35c3cd72da7a9b7672d610b32ebaa4
gunheecha <gunhee2326@naver.com>
2026-02-10 10:25:47 +0900

    refactor : 공통 예외 처리 구조 개선: Interface 기반 에러 코드 패턴 적용 (#6)
    web 의존성 추가
    error code를 인터페이스로 변경
    공통 에러코드 파일을 새로 추가
    에러 응답 형식 변경
    전역 예외 핸들러 추가
    
    Closes : #5

--- src/main/java/kr/gilmok/common/exception/CustomException.java
+++ src/main/java/kr/gilmok/common/exception/CustomException.java
@@ -4,16 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
-    // ErrorCode Enum은 나중에 팀원들이 채우더라도, 필드 타입은 String이나 Enum으로 열어두면 됨
-    // 지금은 단순하게 String code, message로 해두거나, ErrorCode Enum을 빈 껍데기로 만들어도 됨
-
-    private final String code;
-    private final String message;
-
-    public CustomException(String code, String message) {
-        super(message);
-        this.code = code;
-        this.message = message;
-    }
+    private final ErrorCode errorCode; // 핵심: 인터페이스 타입으로 받음
 }
